95da6092feefe425055eee2f76081c687deb2746
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-08-13 02:17:24 +0700

    feat: add `SplashScreen` best practices implementation

--- app/src/main/java/com/fakhry/loonly/SplashScreenActivity.kt
+++ app/src/main/java/com/fakhry/loonly/SplashScreenActivity.kt
@@ -1,11 +1,14 @@
 package com.fakhry.loonly
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_scereen)
+        val intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
