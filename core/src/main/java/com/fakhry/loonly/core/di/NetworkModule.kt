e38f142c87c3380029fdd177af13f9378cac2333
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-11-22 22:22:35 +0700

    feat: implements `SSL Pinning` to more secure Network Transaction

--- core/src/main/java/com/fakhry/loonly/core/di/NetworkModule.kt
+++ core/src/main/java/com/fakhry/loonly/core/di/NetworkModule.kt
@@ -1,12 +1,13 @@
 package com.fakhry.loonly.core.di
 
 import com.fakhry.loonly.core.BuildConfig
-import com.fakhry.loonly.core.data.source.remote.network.ApiService
 import com.fakhry.loonly.core.const.Const
+import com.fakhry.loonly.core.data.source.remote.network.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -18,15 +19,23 @@ import java.util.concurrent.TimeUnit
 class NetworkModule {
 
     @Provides
-    fun provideOkHttpClient(): OkHttpClient =
-        OkHttpClient.Builder()
+    fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add(Const.HOST_NAME_API, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .add(Const.HOST_NAME_API, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(Const.HOST_NAME_API, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
+
+        return OkHttpClient.Builder()
             .apply {
                 if (BuildConfig.DEBUG)
                     this.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             }
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
+    }
 
     @Provides
     fun provideApiService(client: OkHttpClient): ApiService {
